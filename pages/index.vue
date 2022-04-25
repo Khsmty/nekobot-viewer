f95b67bc7f4afcd72055bbf3595f1edb50306d1f
tubuanha <me@taigasaito.org>
2022-04-26 02:13:10 +0900

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,131 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6" style="text-align: center">
+      <v-btn color="secondary" @click="prevImage()">
+        <v-icon>mdi-arrow-left</v-icon> Prev
+      </v-btn>
+      <v-btn color="success" @click="nextImage()">
+        Next <v-icon>mdi-arrow-right</v-icon>
+      </v-btn>
+
+      <div style="margin: 1rem 0"></div>
+
+      <v-img :src="imgURL" height="100vh" contain>
+        <template #placeholder>
+          <v-row class="fill-height ma-0" align="center" justify="center">
+            <v-progress-circular
+              indeterminate
+              color="grey lighten-5"
+            ></v-progress-circular>
+          </v-row>
+        </template>
+      </v-img>
+
+      <div style="margin: 1rem 0"></div>
+
+      <a :href="imgURL" target="_blank">
+        <v-btn color="primary">Download</v-btn>
+      </a>
+
+      <div style="margin: 1rem 0"></div>
+
+      <v-select
+        v-model="autoSec"
+        :items="auto"
+        label="auto (sec)"
+        @change="nextImage()"
+      ></v-select>
+      <v-select
+        v-model="currentType"
+        :items="types"
+        label="type"
+        @change="nextImage()"
+      ></v-select>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+      imgURL: null,
+      nextImgURL: null,
+      history: [],
+      currentType: 'neko',
+      types: [
+        'hass',
+        'hmidriff',
+        'pgif',
+        '4k',
+        'hentai',
+        'holo',
+        'hneko',
+        'neko',
+        'hkitsune',
+        'kemonomimi',
+        'anal',
+        'hanal',
+        'gonewild',
+        'kanna',
+        'ass',
+        'pussy',
+        'thigh',
+        'hthigh',
+        'gah',
+        'coffee',
+        'food',
+        'paizuri',
+        'tentacle',
+        'boobs',
+        'hboobs',
+        'yaoi',
+      ],
+      autoSec: 0,
+      auto: [0, 3, 5, 10, 15, 20, 30, 45, 60],
+    };
+  },
+  async created() {
+    this.imgURL = await this.getImageURL();
+    this.nextImgURL = await this.getImageURL();
+
+    let sec = 0;
+    setInterval(() => {
+      if (this.autoSec) {
+        sec++;
+        if (sec >= this.autoSec) {
+          sec = 0;
+          this.nextImage();
+        }
+      }
+    }, 1000);
+  },
+  methods: {
+    async nextImage() {
+      this.history.unshift(this.imgURL);
+
+      if (this.nextImgURL) {
+        this.imgURL = null;
+        this.imgURL = this.nextImgURL;
+        this.nextImgURL = null;
+      } else {
+        this.imgURL = await this.getImageURL();
+      }
+
+      this.nextImgURL = await this.getImageURL();
+    },
+    prevImage() {
+      this.nextImgURL = this.imgURL;
+      this.imgURL = this.history.shift();
+    },
+    async getImageURL() {
+      const img = await this.$axios.get(
+        `https://nekobot.xyz/api/image?type=${this.currentType}`
+      );
+
+      return img.data.message;
+    },
+  },
+};
+</script>
